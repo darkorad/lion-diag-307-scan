139d0d2f61eca2266aa5af3b5de1036208f4d1fd
darkorad <darkorad@gmail.com>
2025-08-30 20:47:28 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      clientPort: 8080,
-    },
+   server: {
+    port: 3000,
+    host: '0.0.0.0', // Explicitly bind to all interfaces
+    allowedHosts: true
   },
   plugins: [
     react(),
